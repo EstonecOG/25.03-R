10ff83daead96efe905d82b0ae2a36fc146247d3
Kliment <72714002+EstonecOG@users.noreply.github.com>
2022-03-25 08:10:16 -0700

    Add files via upload

---
+++ module1.py
@@ -0,0 +1,7 @@
+﻿def lisa_sõna(Pealinnad:dict, a:str)->dict:
+	"""Lisama sõna sõnavarasse
+	"""
+	a=input("Siseta sõna tähendus >>> ") #просим ввести значение слова, мы знаем ключ из прошлой функции по поиску
+	Capitals.update({a.title():b.title()}) #добавляем в словарь
+	return Pealinnad
+
